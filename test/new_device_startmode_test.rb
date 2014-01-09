5832e8a68b42b1f12c8f0f6619a2fdcd8eebfee7
Michal Filka <mfilka@suse.cz>
2014-01-09 14:32:21 +0100

    Adapted testsuite to changes caused by refactoring.

--- test/new_device_startmode_test.rb
+++ test/new_device_startmode_test.rb
@@ -7,29 +7,28 @@ require "yast"
 Yast.import "ProductFeatures"
 Yast.import "LanItems"
 
-describe "#startmode_for_product" do
+describe "LanItemsClass#new_device_startmode" do
 
   DEVMAP_STARTMODE_INVALID = {
     "STARTMODE" => "invalid"
   }
 
-  context "When product_startmode is auto" do
-
-    it "results to auto" do
-      expect(Yast::ProductFeatures)
-        .to receive(:GetStringFeature)
-        .with("network", "startmode") { "auto" }
-
-      devmap = Yast::deep_copy( DEVMAP_STARTMODE_INVALID)
-      result_devmap = Yast::LanItems.startmode_for_product(devmap)
-      expect(result_devmap).to include("STARTMODE" => "auto")
-    end
-  end
-
   ["hotplug", ""].each do |hwinfo_hotplug|
 
     expected_startmode = hwinfo_hotplug == "hotplug" ? "hotplug" : "auto"
     hotplug_desc = hwinfo_hotplug == "hotplug" ? "can hotplug" : "cannot hotplug"
+
+    context "When product_startmode is auto and device " + hotplug_desc do
+
+      it "results to auto" do
+        expect(Yast::ProductFeatures)
+          .to receive(:GetStringFeature)
+          .with("network", "startmode") { "auto" }
+
+        result = Yast::LanItems.new_device_startmode
+        expect(result).to be_eql "auto"
+      end
+    end
 
     context "When product_startmode is ifplugd and device " + hotplug_desc do
 
@@ -37,7 +36,7 @@ describe "#startmode_for_product" do
         expect(Yast::ProductFeatures)
           .to receive(:GetStringFeature)
           .with("network", "startmode") { "ifplugd" }
-        Yast::Ops.stub(:get_string) { hwinfo_hotplug }
+        Yast::LanItems.stub(:hotplug_usable?) { hwinfo_hotplug == "hotplug" }
         # setup stubs by default at results which doesn't need special handling
         Yast::Arch.stub(:is_laptop) { true }
         Yast::NetworkService.stub(:is_network_manager) { false }
@@ -47,27 +46,24 @@ describe "#startmode_for_product" do
         expect(Yast::Arch)
           .to receive(:is_laptop) { false }
 
-        devmap = Yast::deep_copy( DEVMAP_STARTMODE_INVALID)
-        result_devmap = Yast::LanItems.startmode_for_product(devmap)
-        expect(result_devmap).to include("STARTMODE" => expected_startmode)
+        result = Yast::LanItems.new_device_startmode
+        expect(result).to be_eql expected_startmode
       end
 
       it "results to ifplugd when running on laptop" do
         expect(Yast::Arch)
           .to receive(:is_laptop) { true }
 
-        devmap = Yast::deep_copy( DEVMAP_STARTMODE_INVALID)
-        result_devmap = Yast::LanItems.startmode_for_product(devmap)
-        expect(result_devmap).to include("STARTMODE" => "ifplugd")
+        result = Yast::LanItems.new_device_startmode
+        expect(result).to be_eql "ifplugd"
       end
 
       it "results to #{expected_startmode} when running NetworkManager" do
         expect(Yast::NetworkService)
           .to receive(:is_network_manager) { true }
 
-        devmap = Yast::deep_copy( DEVMAP_STARTMODE_INVALID)
-        result_devmap = Yast::LanItems.startmode_for_product(devmap)
-        expect(result_devmap).to include("STARTMODE" => expected_startmode)
+        result = Yast::LanItems.new_device_startmode
+        expect(result).to be_eql expected_startmode
       end
 
       it "results to #{expected_startmode} when current device is virtual one" do
@@ -76,9 +72,8 @@ describe "#startmode_for_product" do
         # other calls of the function. It might have unexpected inpacts.
         Yast::LanItems.stub(:type) { "bond" }
 
-        devmap = Yast::deep_copy( DEVMAP_STARTMODE_INVALID)
-        result_devmap = Yast::LanItems.startmode_for_product(devmap)
-        expect(result_devmap).to include("STARTMODE" => expected_startmode)
+        result = Yast::LanItems.new_device_startmode
+        expect(result).to be_eql expected_startmode
       end
     end
 
@@ -93,15 +88,15 @@ describe "#startmode_for_product" do
 
       AVAILABLE_PRODUCT_STARTMODES.each do |product_startmode|
 
-        # defaults are set elsewhere currently in such case
-        it "for #{product_startmode} it does nothing if device " + hotplug_desc do
+        it "for #{product_startmode} it results to #{expected_startmode} if device " + hotplug_desc do
           expect(Yast::ProductFeatures)
             .to receive(:GetStringFeature)
             .with("network", "startmode") { product_startmode }
+          expect(Yast::LanItems)
+            .to receive(:hotplug_usable?) { hwinfo_hotplug == "hotplug" }
 
-          devmap = Yast::deep_copy( DEVMAP_STARTMODE_INVALID)
-          result_devmap = Yast::LanItems.startmode_for_product(devmap)
-          expect(result_devmap).to be_eql DEVMAP_STARTMODE_INVALID
+          result = Yast::LanItems.new_device_startmode
+          expect(result).to be_eql expected_startmode
         end
       end
     end
