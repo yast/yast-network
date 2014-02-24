af24c37f82a3045c7a104c1bbcfd7396b6e9c17f
Michal Filka <mfilka@suse.cz>
2014-02-24 12:06:10 +0100

    Cleaning the code in InstInstallInfClient.

--- src/clients/inst_install_inf.rb
+++ src/clients/inst_install_inf.rb
@@ -38,124 +38,6 @@ module Yast
       Yast.include self, "network/complex.rb"
     end
 
-    def dev_name
-      netdevice = InstallInf["Netdevice"]
-
-      if Mode.autoinst
-        # if possible, for temporary installation network use same device
-        # with same MAC address (even if devicename changed) (bnc#648270)
-        new_devname = LanUdevAuto.GetDevnameByMAC(InstallInf["HWAddr"])
-
-        netdevice = new_devname if !new_devname.empty?
-      end
-
-      Builtins.y2milestone("InstInstallInfClient#dev_name:%1", netdevice)
-      return netdevice
-    end
-
-    def StdoutOf(command)
-      SCR.Execute(path(".target.bash_output"), command)["stdout"].to_s
-    end
-
-    def create_wlan_ifcfg
-      wlan_key = InstallInf["WlanKey"]
-      wlan_essid = InstallInf["WlanESSID"]
-      wlan_key_type = InstallInf["WlanKeyType"]
-      wlan_auth = InstallInf["WlanAuth"]
-
-      return "" if wlan_essid.empty?
-
-      ifcfg = ""
-      ifcfg << "WIRELESS_ESSID='%s'\n" % wlan_essid
-
-      case wlan_auth
-        when "", "psk"
-          ifcfg << "WIRELESS_WPA_PSK='%s'\n" % wlan_key
-          ifcfg << "WIRELESS_AUTH_MODE='psk'\n"
-
-        when "open"
-          ifcfg << "WIRELESS_AUTH_MODE='no-encryption'\n"
-
-        when "wep_open", "wep_restricted"
-          type = ""
-          if wlan_key_type == "password"
-            type = "h:"
-          elsif wlan_key_type == "ascii"
-            type = "s:"
-          end
-
-          ifcfg << "WIRELESS_AUTH_MODE='%s'\n" % wlan_auth == "wep-open" ? "open" : "sharedkey"
-          ifcfg << "WIRELESS_DEFAULT_KEY='0'\n"
-          ifcfg << "WIRELESS_KEY_0='%s%s'\n" % [type, wlan_key]
-          ifcfg << "WIRELESS_KEY_LENGTH='%s'\n" % InstallInf["WlanKeyLen"]
-
-          if !wlan_key_type.empty? && !wlan_key.empty?
-            ifcfg = "WIRELESS_KEY_0='%s:%s'\n" % [
-              Builtins.substring(wlan_key_type, 0, 1),
-              wlan_key
-            ]
-          end
-      end
-
-      return ifcfg
-    end
-
-    def create_s390_ifcfg(hardware)
-      hwaddr = InstallInf["HWAddr"]
-      netdevice = dev_name
-      devtype = NetworkInterfaces.GetType(netdevice) if !netdevice.empty?
-
-      Builtins.y2milestone("Interface type: %1", devtype)
-
-      # only some card types need a persistent MAC (bnc#658708)
-      sysfs_id = dev_name_to_sysfs_id(netdevice, hardware)
-      hwaddr = "" if !s390_device_needs_persistent_mac(sysfs_id, hardware)
-
-      # hsi devices do not support setting hwaddr (bnc #479481)
-      hwaddr = "" if devtype == "hsi"
-
-      # set HW address only for qeth set to Layer 2 (bnc #479481)
-      hwaddr = "" if devtype == "eth" && InstallInf["QETH_LAYER2_SUPPORT"] != "1"
-
-      return "LLADDR='%s'\n" % hwaddr if !hwaddr.empty?
-    end
-
-    def create_device_name_ifcfg(hardware)
-      device_name = dev_name
-
-      hw_name = BuildDescription(
-        NetworkInterfaces.device_type(device_name),
-        NetworkInterfaces.device_num(device_name),
-        { "dev_name" => device_name },
-        hardware
-      )
-
-      # protect special characters, #305343
-      return "NAME='%s'\n" % String.Quote(hw_name) if !hw_name.empty?
-    end
-
-    def write_ifcfg(ifcfg)
-      device_name = dev_name
-
-      if !LanUdevAuto.AllowUdevModify
-        # bnc#821427: use same options as in /lib/udev/rules.d/71-biosdevname.rules
-        cmd = "biosdevname --policy physical --smbios 2.6 --nopirq -i %s" % dev_name
-        out = String.FirstChunk(StdoutOf(cmd), "\n")
-        if !out.empty?
-          device_name = out
-          Builtins.y2milestone("biosdevname renames #{dev_name} to #{device_name}")
-        end
-      end
-
-      ifcfg_name = "ifcfg-%s" % device_name
-
-      # write only if file doesn't exists
-      dev_file = Builtins.sformat("/etc/sysconfig/network/%1", ifcfg_name)
-
-      SCR.Write(path(".target.string"), dev_file, ifcfg)
-      Builtins.y2milestone("ifcfg file: %1", dev_file)
-    end
-
     def CreateIfcfg
       ifcfg = ""
 
@@ -182,8 +64,8 @@ module Yast
 
           ip6_addr = InstallInf["IP6"]
           if !ip6_addr.empty?
-            ifcfg << "%s" % "LABEL_ipv6='ipv6'\n"
-            ifcfg << "IPADDR_ipv6='%s'\n" % ip6_addr
+            ifcfg << "LABEL_ipv6='ipv6'\n"
+            ifcfg << "IPADDR_ipv6='#{ip6_addr}'\n"
           end
 
         when "dhcp"
@@ -200,7 +82,7 @@ module Yast
         if bootproto =~ /dhcp/
           Builtins.y2milestone("set DHCLIENT_SET_HOSTNAME=yes on installed system")
           SCR.Execute(
-            path(".target.bash"),
+            BASH_PATH,
             "sed -i s/\"DHCLIENT_SET_HOSTNAME=.*\"/'DHCLIENT_SET_HOSTNAME=\"yes\"'/g /etc/sysconfig/network/dhcp"
           )
         end
@@ -213,7 +95,7 @@ module Yast
 
       # if available, write MTU
       mtu = InstallInf["IP_MTU"]
-      ifcfg << "MTU='%s'\n" % mtu if !mtu.empty?
+      ifcfg << "MTU='#{mtu}'\n" if !mtu.empty?
 
       # for qeth devices (s390)
       # bnc#578689 - YaST2 should not write the MAC address into ifcfg file
@@ -224,7 +106,7 @@ module Yast
 
       # point to point interface
       remote_ip = InstallInf["Pointopoint"]
-      ifcfg << "REMOTE_IPADDR='%s'\n" % remote_ip if !remote_ip.empty?
+      ifcfg << "REMOTE_IPADDR='#{remote_ip}'\n" if !remote_ip.empty?
 
       ifcfg << create_device_name_ifcfg(hardware)
 
@@ -263,47 +145,51 @@ module Yast
         SCR.Write(path(".target.string"), "/etc/HOSTNAME", hostname)
       end
 
+      gateway = InstallInf["Gateway"]
+      ptp =  InstallInf["Pointopoint"]
       if InstallInf["NetConfig"] == "static"
         # create routes file
-        if !InstallInf["Gateway"].empty?
+        if !gateway.empty?
           Builtins.y2milestone(
             "Writing route : %1",
-            InstallInf["Gateway"]
+            gateway
           )
           SCR.Write(
             path(".target.string"),
             "/etc/sysconfig/network/routes",
-            "default %s - -\n" % InstallInf["Gateway"]
+            "default %s - -\n" % gateway
           )
-        elsif InstallInf["Pointopoint"]
+        elsif ptp
           Builtins.y2milestone(
             "Writing Peer-to-Peer route: %1",
-            InstallInf["Pointopoint"]
+            ptp
           )
           SCR.Write(
             path(".target.string"),
             "/etc/sysconfig/network/routes",
-            "default %s - -\n" % InstallInf["Pointopoint"]
+            "default %s - -\n" % ptp
           )
         else
           Builtins.y2warning("No routing information in install.inf")
         end
 
         # write DHCPTimeout linuxrc parameter as /etc/sysconfig/network/config.WAIT_FOR_INTERFACES (bnc#396824)
-        if InstallInf["DHCPTimeout"]
+        dhcp_timeout = InstallInf["DHCPTimeout"]
+        if dhcp_timeout
           SCR.Write(
             path(".sysconfig.network.config.WAIT_FOR_INTERFACES"),
-            InstallInf["DHCPTimeout"]
+            dhcp_timeout
           )
           Builtins.y2milestone(
             "Writing WAIT_FOR_INTERFACES=%1",
-            InstallInf["DHCPTimeout"]
+            dhcp_timeout
           )
         end
 
         # create resolv.conf only for static configuration
-        if InstallInf["Nameserver"]
-          serverlist = InstallInf["Nameserver"]
+        nameserver = InstallInf["Nameserver"]
+        if nameserver
+          serverlist = nameserver
           # write also secondary and third nameserver when available (bnc#446101)
           nameserver2 = InstallInf["Nameserver2"]
           serverlist << " " << nameserver2 if !nameserver2.empty?
@@ -390,12 +276,130 @@ module Yast
       connect_wait = InstallInf["ConnectWait"]
       if !connect_wait.empty?
         SCR.Execute(
-          path(".target.bash_output"),
+          BASH_PATH,
           "sed -i s/^WAIT_FOR_INTERFACES=.*/WAIT_FOR_INTERFACES=%s/g /etc/sysconfig/network/config" % connect_wait
         )
       end
 
       nil
+    end
+
+    private
+    def dev_name
+      netdevice = InstallInf["Netdevice"]
+
+      if Mode.autoinst
+        # if possible, for temporary installation network use same device
+        # with same MAC address (even if devicename changed) (bnc#648270)
+        new_devname = LanUdevAuto.GetDevnameByMAC(InstallInf["HWAddr"])
+
+        netdevice = new_devname if !new_devname.empty?
+      end
+
+      Builtins.y2milestone("InstInstallInfClient#dev_name:%1", netdevice)
+      return netdevice
+    end
+
+    def StdoutOf(command)
+      SCR.Execute(path(".target.bash_output"), command)["stdout"].to_s
+    end
+
+    def create_wlan_ifcfg
+      wlan_key = InstallInf["WlanKey"]
+      wlan_essid = InstallInf["WlanESSID"]
+      wlan_key_type = InstallInf["WlanKeyType"]
+      wlan_auth = InstallInf["WlanAuth"]
+      wlan_key_len = InstallInf["WlanKeyLen"]
+
+      return "" if wlan_essid.empty?
+
+      ifcfg = "WIRELESS_ESSID='#{wlan_essid}'\n"
+
+      case wlan_auth
+        when "", "psk"
+          ifcfg << "WIRELESS_WPA_PSK='#{wlan_key}'\n"
+          ifcfg << "WIRELESS_AUTH_MODE='psk'\n"
+
+        when "open"
+          ifcfg << "WIRELESS_AUTH_MODE='no-encryption'\n"
+
+        when "wep_open", "wep_restricted"
+          if wlan_key_type == "password"
+            type = "h:"
+          elsif wlan_key_type == "ascii"
+            type = "s:"
+          else
+            type = wlan_key_type[0] + ":"
+          end
+
+          auth_mode = wlan_auth == "wep-open" ? "open" : "sharedkey"
+
+          ifcfg << "WIRELESS_AUTH_MODE='%s'\n" % wlan_auth_mode
+          ifcfg << "WIRELESS_DEFAULT_KEY='0'\n"
+          ifcfg << "WIRELESS_KEY_0='#{type}#{wlan_key}'\n"
+          ifcfg << "WIRELESS_KEY_LENGTH='#{wlan_key_len}'\n"
+      end
+
+      return ifcfg
+    end
+
+    def create_s390_ifcfg(hardware)
+      hwaddr = InstallInf["HWAddr"]
+      return "" if hwaddr.empty?
+
+      netdevice = dev_name
+      return "" if netdevice.empty?
+
+      devtype = NetworkInterfaces.GetType(netdevice)
+
+      Builtins.y2milestone("Interface type: %1", devtype)
+
+      # only some card types need a persistent MAC (bnc#658708)
+      sysfs_id = dev_name_to_sysfs_id(netdevice, hardware)
+      return "" if !s390_device_needs_persistent_mac(sysfs_id, hardware)
+
+      # hsi devices do not support setting hwaddr (bnc #479481)
+      return "" if devtype == "hsi"
+
+      # set HW address only for qeth set to Layer 2 (bnc #479481)
+      return "" if devtype == "eth" && InstallInf["QETH_LAYER2_SUPPORT"] != "1"
+
+      return "LLADDR='#{hwaddr}'\n"
+    end
+
+    def create_device_name_ifcfg(hardware)
+      device_name = dev_name
+
+      hw_name = BuildDescription(
+        NetworkInterfaces.device_type(device_name),
+        NetworkInterfaces.device_num(device_name),
+        { "dev_name" => device_name },
+        hardware
+      )
+
+      return "" if hw_name.empty?
+
+      # protect special characters, #305343
+      return "NAME='%s'\n" % String.Quote(hw_name)
+    end
+
+    def write_ifcfg(ifcfg)
+      device_name = dev_name
+
+      if !LanUdevAuto.AllowUdevModify
+        # bnc#821427: use same options as in /lib/udev/rules.d/71-biosdevname.rules
+        cmd = "biosdevname --policy physical --smbios 2.6 --nopirq -i %s" % dev_name
+        out = String.FirstChunk(StdoutOf(cmd), "\n")
+        if !out.empty?
+          device_name = out
+          Builtins.y2milestone("biosdevname renames #{dev_name} to #{device_name}")
+        end
+      end
+
+      dev_file = "/etc/sysconfig/network/ifcfg-#{device_name}"
+
+      Builtins.y2milestone("ifcfg file: %1", dev_file)
+      SCR.Write(path(".target.string"), dev_file, ifcfg)
     end
 
   end
