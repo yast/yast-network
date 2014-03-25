12e7f6a9a587a932923834217e7d4960ad9e303b
Michal Filka <mfilka@suse.cz>
2014-03-25 20:18:44 +0100

    Refactoring: replaced proprietary timed dialog in ConfirmVirtProposal
    using Popup.TimedAnyQuestion. It simplifies and DRY the code. Moreover
    user now is informed that dialog has timeout (it wasn't clear before)

--- src/lib/network/confirm_virt_proposal.rb
+++ src/lib/network/confirm_virt_proposal.rb
@@ -4,93 +4,35 @@ require 'yast'
 
 module Yast
 
-  Yast.import "UI"
-  Yast.import "LanItems"
-  Yast.import "Popup"
-
   # The class represents a simple dialog which asks user for confirmation of
   # network.service restart during installation.
   class ConfirmVirtProposal
 
+    include Singleton
     include UIShortcuts
-    extend I18n
+    include I18n
 
-    def self.run
-      open
+    Yast.import "Popup"
+    Yast.import "Label"
 
-      # for autoinstallation popup has timeout 10 seconds (#192181)
-      # timeout for every case (bnc#429562)
-      ret = UI.TimeoutUserInput(10 * 1000)
-
-      close
-
-      ret
-    end
-
-  private
-
-    def self.open
+    # Shows a confirmation timed dialogue
+    #
+    # Returns :ok when user agreed, :cancel otherwise
+    def run
       textdomain "network"
 
-      UI.OpenDialog(
-        Opt(:decorated),
-        HBox(
-          HSpacing(1),
-          HCenter(
-            HSquash(
-              VBox(
-                HCenter(
-                  HSquash(
-                    VBox(
-                      # This is the heading of the popup box
-                      Left(Heading(_("Confirm Network Restart"))),
-                      VSpacing(0.5),
-                      # This is in information message. Next come the
-                      # hardware class name (network cards).
-                      HVCenter(
-                        Label(
-                          _(
-                            "Because of the bridged network, YaST2 needs to restart the network to apply the settings."
-                          )
-                        )
-                      ),
-                      VSpacing(0.5)
-                    )
-                  )
-                ),
-                ButtonBox(
-                  HWeight(
-                    1,
-                    PushButton(
-                      Id(:ok),
-                      Opt(:default, :okButton),
-                      Label.OKButton
-                    )
-                  ),
-                  # PushButton label
-                  HWeight(
-                    1,
-                    PushButton(
-                      Id(:cancel),
-                      Opt(:cancelButton),
-                      Label.CancelButton
-                    )
-                  )
-                ),
-                VSpacing(0.2)
-              )
-            )
-          ),
-          HSpacing(1)
-        )
+      ret = Popup.TimedAnyQuestion(
+        _("Confirm Network Restart"),
+        _(
+          "Because of the bridged network, YaST2 needs to restart the network to apply the settings."
+        ),
+        Label.OKButton,
+        Label.CancelButton,
+        :focus_yes,
+        10
       )
 
-      UI.SetFocus(Id(:ok))
+      ret ? :ok : :cancel
     end
-
-    def self.close
-      UI.CloseDialog
-    end
-
   end
 end
