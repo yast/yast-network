85cbb613f274cfea79ea6334eb611da3e0dc397c
Michal Filka <mfilka@suse.cz>
2014-08-27 16:11:32 +0200

    Updated testsuite. Handles installer as a special case

--- test/complex_test.rb
+++ test/complex_test.rb
@@ -8,6 +8,7 @@ include Yast::UIShortcuts
 include Yast::I18n
 
 Yast.import "LanItems"
+Yast.import "Stage"
 
 # creating a wrapper for Yast's 'header' file
 $LOAD_PATH.unshift File.expand_path('../../src', __FILE__)
@@ -19,26 +20,51 @@ end
 
 describe "NetworkLanComplexInclude::input_done?" do
 
-  it "returns true for input different than :abort" do
-    expect(NetworkLanComplexInclude.input_done?(:no_abort)).to eql true
+  context "when not running in installer" do
+
+    before(:each) do
+      allow(Yast::Stage)
+        .to receive(:initial)
+        .and_return(false)
+    end
+
+    it "returns true for input different than :abort" do
+      expect(NetworkLanComplexInclude.input_done?(:no_abort)).to eql true
+    end
+
+    it "returns true for input equal to :abort in case of no user modifications" do
+      allow(Yast::LanItems)
+        .to receive(:modified)
+        .and_return(false)
+
+      expect(NetworkLanComplexInclude.input_done?(:abort)).to eql true
+    end
+
+    it "asks user for abort confirmation for input equal to :abort and user did modifications" do
+      allow(Yast::LanItems)
+        .to receive(:modified)
+        .and_return(true)
+
+      expect(NetworkLanComplexInclude)
+        .to receive(:ReallyAbort)
+
+      NetworkLanComplexInclude.input_done?(:abort)
+    end
   end
 
-  it "returns true for input equal to :abort in case of no user modifications" do
-    allow(Yast::LanItems)
-      .to receive(:modified)
-      .and_return(false)
+  context "when running in installer" do
 
-    expect(NetworkLanComplexInclude.input_done?(:abort)).to eql true
-  end
+    before(:each) do
+      allow(Yast::Stage)
+        .to receive(:initial)
+        .and_return(true)
+    end
 
-  it "asks user for abort confirmation for input equal to :abort and user did modifications" do
-    allow(Yast::LanItems)
-      .to receive(:modified)
-      .and_return(true)
+    it "asks user for installation abort confirmation for input equal to :abort" do
+      expect(Yast::Popup)
+        .to receive(:ConfirmAbort)
 
-    expect(NetworkLanComplexInclude)
-      .to receive(:ReallyAbort)
-
-    NetworkLanComplexInclude.input_done?(:abort)
+      NetworkLanComplexInclude.input_done?(:abort)
+    end
   end
 end
