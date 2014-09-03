0dceb30ec0fc3332d56bb4475f298fcea95e800d
Martin Vidner <mvidner@suse.cz>
2014-09-03 10:30:04 +0200

    test: Stubed out sleep(5).
    Now they can all run in 4s total.

--- test/link_handlers_test.rb
+++ test/link_handlers_test.rb
@@ -4,20 +4,29 @@ require_relative "test_helper"
 
 require "yast"
 
-describe "phy_connected?" do
-  before(:each) do
+# need a class to stub the sleep call; hard to stub it on Kernel
+class LinkHandlersClass
+  def initialize
     Yast.include self, "network/routines.rb"
+  end
+end
 
+describe "phy_connected?" do
+  include Yast
+  subject { LinkHandlersClass.new }
+
+  before(:each) do
     Yast::SCR.stub(:Execute).with(path(".target.bash"), //) { 0 }
+    allow(subject).to receive(:sleep)
   end
 
   it "returns true if PHY layer is available" do
     Yast::SCR.stub(:Read).with(path(".target.string"), /\/sys\/class\/net/) { 1 }
-    expect(phy_connected?("enp0s3")).to eql true
+    expect(subject.phy_connected?("enp0s3")).to eql true
   end
 
   it "returns false if PHY layer is available" do
     Yast::SCR.stub(:Read).with(path(".target.string"), /\/sys\/class\/net/) { 0 }
-    expect(phy_connected?("enp0s3")).to eql false
+    expect(subject.phy_connected?("enp0s3")).to eql false
   end
 end
