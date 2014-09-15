db8a8c412739f529be3f7a85281af437fcf08257
Martin Vidner <mvidner@suse.cz>
2014-09-15 15:43:53 +0200

    test SetDeviceVars SetS390Vars

---
+++ test/lan_items_read_test.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env rspec
+
+require_relative "test_helper"
+
+require "yast"
+Yast.import "LanItems"
+
+describe "LanItemsClass" do
+  subject(:li) { Yast::LanItems }
+
+  describe "#SetDeviceVars" do
+    let(:defaults) { { } }
+
+    it "reads value from sysconfig data" do
+      li.SetDeviceVars({"BOOTPROTO" => "dhcp8"}, {"BOOTPROTO" => "dhcp7"})
+      expect(li.bootproto).to eq "dhcp8"
+    end
+
+    it "reads value from default data" do
+      li.SetDeviceVars({}, {"BOOTPROTO" => "dhcp7"})
+      expect(li.bootproto).to eq "dhcp7"
+    end
+
+    it "reads nil if neither hash specifies the data" do
+      li.SetDeviceVars({}, {})
+      expect(li.bootproto).to eq nil
+    end
+
+    it "converts set_default_route" do
+      li.SetDeviceVars({"DHCLIENT_SET_DEFAULT_ROUTE" => "yes"}, defaults)
+      expect(li.set_default_route).to eq true
+
+      li.SetDeviceVars({"DHCLIENT_SET_DEFAULT_ROUTE" => "no"}, defaults)
+      expect(li.set_default_route).to eq false
+
+      li.SetDeviceVars({}, defaults)
+      expect(li.set_default_route).to eq nil
+
+      li.SetDeviceVars({"DHCLIENT_SET_DEFAULT_ROUTE" => "unrecognized"}, defaults)
+      expect(li.set_default_route).to eq nil
+    end
+
+    it "converts wl_power" do
+      li.SetDeviceVars({"WIRELESS_POWER" => "yes"}, defaults)
+      expect(li.wl_power).to eq true
+    end
+
+  end
+
+  describe "#SetS390Vars" do
+    let(:defaults) { { } }
+
+    it "converts qeth_layer2" do
+      expect(Yast::Arch).to receive(:s390).and_return true
+
+      li.SetS390Vars({"QETH_LAYER2" => "yes"}, defaults)
+      expect(li.qeth_layer2).to eq true
+    end
+
+  end
+end
