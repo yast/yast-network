beaeba1826e475861c0c8cb3e26d71481a94810a
Ancor Gonzalez Sosa <ancor@suse.de>
2014-09-30 12:12:06 +0200

    Use control.xml to set DNS.dhcp_hostname (bnc#870896)
    In order to completely close the bug, changes in control.xml for both SLED and
    SLES are needed.

---
+++ test/dns_test.rb
@@ -0,0 +1,111 @@
+#!/usr/bin/env rspec
+
+ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
+
+require "yast"
+require_relative "SCRStub"
+
+module Yast
+  RSpec.configure do |c|
+    c.include SCRStub
+  end
+
+  import "Arch"
+  import "DNS"
+  import "ProductControl"
+
+  describe DNS do
+    describe ".default_dhcp_hostname" do
+      before do
+        allow(Arch).to receive(:is_laptop).and_return laptop
+        ProductControl.ReadControlFile(File.join(SCRStub::DATA_PATH, control_file))
+      end
+
+      context "with dhcp_hostname=true in control file" do
+        let(:control_file) { "dhcp_hostname_true.xml" }
+
+        context "in a laptop" do
+          let(:laptop) { true }
+
+          it "returns the value from product features" do
+            expect(DNS.default_dhcp_hostname).to eql(true)
+          end
+        end
+
+        context "in a workstation" do
+          let(:laptop) { false }
+
+          it "returns the value from product features" do
+            expect(DNS.default_dhcp_hostname).to eql(true)
+          end
+        end
+      end
+
+      context "with dhcp_hostname=false in control file" do
+        let(:control_file) { "dhcp_hostname_false.xml" }
+
+        context "in a laptop" do
+          let(:laptop) { true }
+
+          it "returns the value from product features" do
+            expect(DNS.default_dhcp_hostname).to eql(false)
+          end
+        end
+
+        context "in a workstation" do
+          let(:laptop) { false }
+
+          it "returns the value from product features" do
+            expect(DNS.default_dhcp_hostname).to eql(false)
+          end
+        end
+      end
+
+      context "without dhcp_hostname in control file" do
+        let(:control_file) { "dhcp_hostname_nil.xml" }
+
+        context "in a laptop" do
+          let(:laptop) { true }
+
+          it "returns false" do
+            expect(DNS.default_dhcp_hostname).to eql(false)
+          end
+        end
+
+        context "in a workstation" do
+          let(:laptop) { false }
+
+          it "returns true" do
+            expect(DNS.default_dhcp_hostname).to eql(true)
+          end
+        end
+      end
+    end
+
+    describe ".Import" do
+      context "with present dhcp_hostname and write_hostname" do
+        let(:settings) { {"hostname" => "host", "dhcp_hostname" => true, "write_hostname" => true} }
+
+        it "honors the provided values" do
+          expect(DNS).to_not receive(:DefaultWriteHostname)
+          expect(DNS).to_not receive(:default_dhcp_hostname)
+          DNS.Import(settings)
+          expect(DNS.write_hostname).to eql(true)
+          expect(DNS.dhcp_hostname).to eql(true)
+        end
+      end
+
+      context "with missing dhcp_hostname and write_hostname" do
+        let(:settings) { {"hostname" => "host"} }
+
+        it "relies on proper methods to get default values" do
+          expect(DNS).to receive(:DefaultWriteHostname).and_return false
+          expect(DNS).to receive(:default_dhcp_hostname).and_return false
+          DNS.Import(settings)
+          expect(DNS.write_hostname).to eql(false)
+          expect(DNS.dhcp_hostname).to eql(false)
+        end
+      end
+    end
+  end
+end
