623d0311934022e94c4247759e96a4f6c18745df
Michal Filka <mfilka@suse.cz>
2015-05-27 15:52:42 +0200

    Fixed docs

--- src/lib/network/network_autoyast.rb
+++ src/lib/network/network_autoyast.rb
@@ -8,20 +8,20 @@ module Yast
     # Merges two devices map into one.
     #
     # Maps are expected in NetworkInterfaces format. That is
-    # $[
-    #  type1:
-    #    $[
-    #      dev_name_1: $[ ... ],
-    #      ...
-    #     ],
+    # {
+    #   type1:
+    #   {
+    #     dev_name_1: { ... },
+    #     ...
+    #   },
     #   ...
-    # ]
+    # }
     #
     # If a device definition is present in both maps, then the one from devices2
     # wins.
     #
-    # @param [Hash] in_devs1 first map of devices in NetworkInterfaces format
-    # @param [Hash] in_devs2 second map of devices in NetworkInterfaces format
+    # @param [Hash, nil] in_devs1 first map of devices in NetworkInterfaces format
+    # @param [Hash, nil] in_devs2 second map of devices in NetworkInterfaces format
     #
     # @return merged device map in NetworkInterfaces format or empty map
     def merge_devices(in_devs1, in_devs2)
