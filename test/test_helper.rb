99aad74e58fbf8898a90c1cc5278bc051c320644
Michal Filka <mfilka@suse.cz>
2015-06-02 10:02:08 +0200

    Enabled CodeCoverage support

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,25 @@
 srcdir = File.expand_path("../../src", __FILE__)
 y2dirs = ENV.fetch("Y2DIR", "").split(":")
 ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
+
+require "yast"
+
+if ENV["COVERAGE"]
+  require "simplecov"
+  SimpleCov.start
+
+  # For coverage we need to load all ruby files
+  # Note that clients/ are excluded because they run too eagerly by design
+  Dir["#{srcdir}/{include,modules}/**/*.rb"].each do |f|
+    require_relative f
+  end
+
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
+end
