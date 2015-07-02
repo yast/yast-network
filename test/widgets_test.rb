31f8eecaae06f1ca14a3ca0c2489b096ceead897
Michal Filka <mfilka@suse.cz>
2015-07-02 11:59:22 +0200

    Fixed widgets test to work with coverage

---
+++ test/widgets_test.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env rspec
+
+require_relative "test_helper"
+
+require "yast"
+
+include Yast::UIShortcuts
+include Yast::I18n
+
+Yast.import "LanItems"
+Yast.include self, "network/widgets.rb"
+
+describe "NetworkWidgetsInclude::ipoib_mode_widget" do
+  it "contains known IPoIB modes" do
+    widget_def = ipoib_mode_widget
+    expect(widget_def).to include("items")
+
+    expect(widget_def["items"]).to be_eql Yast::LanItems.ipoib_modes.to_a
+  end
+end
