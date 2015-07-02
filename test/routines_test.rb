5cde1a96eaedd5c5a5a03dff1007ed3563f54cad
Michal Filka <mfilka@suse.cz>
2015-07-02 12:53:37 +0200

    Fixed routines test to work with coverage

--- test/routines_test.rb
+++ test/routines_test.rb
@@ -1,10 +1,8 @@
 #! /usr/bin/env rspec
 
-ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
+require_relative "test_helper"
 
 require "yast"
-
-include Yast
 
 Yast.include self, "network/routines.rb"
 
@@ -15,22 +13,22 @@ describe "#PackagesInstall" do
   context "when list of packages is empty" do
     it "returns :next without checking anything" do
       expect(PackagesInstall([])).to eq(:next)
-      expect(Package).not_to receive(:InstalledAll)
+      expect(Yast::Package).not_to receive(:InstalledAll)
     end
   end
 
   context "in inst-sys" do
     it "returns :next without checking anything" do
-      allow(Stage).to receive(:stage).and_return("initial")
+      allow(Yast::Stage).to receive(:stage).and_return("initial")
       expect(PackagesInstall(["1", "2", "3"])).to eq(:next)
-      expect(Package).not_to receive(:InstalledAll)
+      expect(Yast::Package).not_to receive(:InstalledAll)
     end
   end
 
   context "on a running system" do
     it "checks whether all packages are installed and returns a symbol :next or :abort" do
-      allow(Stage).to receive(:stage).and_return("normal")
-      expect(Package).to receive(:InstalledAll).and_return(true)
+      allow(Yast::Stage).to receive(:stage).and_return("normal")
+      expect(Yast::Package).to receive(:InstalledAll).and_return(true)
       expect(PackagesInstall(["1", "2", "3"])).to eq(:next)
     end
   end
