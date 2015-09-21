b8f1a7cbc71e725910224cbd9884bbbbd745f4b5
Michal Filka <mfilka@suse.cz>
2015-09-21 09:07:33 +0200

    Dropped Builtins from Lan#Import

--- src/modules/Lan.rb
+++ src/modules/Lan.rb
@@ -666,34 +666,23 @@ module Yast
     # @param [Hash] settings settings to be imported
     # @return true on success
     def Import(settings)
-      settings = deep_copy(settings)
-      NetworkInterfaces.Import("netcard", Ops.get_map(settings, "devices", {}))
-      Builtins.foreach(NetworkInterfaces.List("netcard")) do |device|
+      settings = {} if settings.nil?
+
+      NetworkInterfaces.Import("netcard", settings["devices"] || {})
+      NetworkInterfaces.List("netcard").each do |device|
         LanItems.AddNew
-        Ops.set(LanItems.Items, LanItems.current, { "ifcfg" => device })
+        LanItems.Items[LanItems.current] = { "ifcfg" => device }
       end
 
-      Ops.set(
-        LanItems.autoinstall_settings,
-        "start_immediately",
-        Ops.get_boolean(settings, "start_immediately", false)
-      )
-      Ops.set(
-        LanItems.autoinstall_settings,
-        "strict_IP_check_timeout",
-        Ops.get_integer(settings, "strict_IP_check_timeout", -1)
-      )
-      Ops.set(
-        LanItems.autoinstall_settings,
-        "keep_install_network",
-        Ops.get_boolean(settings, "keep_install_network", false)
-      )
+      LanItems.autoinstall_settings["start_immediately"] = settings["start_immediately"] || false
+      LanItems.autoinstall_settings["strict_IP_check_timeout"] = settings["strict_IP_check_timeout"] || -1
+      LanItems.autoinstall_settings["keep_install_network"] = settings["keep_install_network"] || false
 
-      NetworkConfig.Import(Ops.get_map(settings, "config", {}))
-      DNS.Import(Builtins.eval(Ops.get_map(settings, "dns", {})))
-      Routing.Import(Builtins.eval(Ops.get_map(settings, "routing", {})))
+      NetworkConfig.Import(settings["config"] || {})
+      DNS.Import(settings["dns"] || {})
+      Routing.Import(settings["routing"] || {})
 
-      if Ops.get_boolean(settings, "managed", false)
+      if settings["managed"]
         if NetworkService.is_backend_available(:network_manager)
           NetworkService.use_network_manager
         else
@@ -703,11 +692,10 @@ module Yast
       else
         NetworkService.use_wicked
       end
-      if Builtins.haskey(settings, "ipv6")
-        @ipv6 = Ops.get_boolean(settings, "ipv6", true)
-      end
 
-      LanItems.modified = true
+      @ipv6 = settings.fetch("ipv6", true)
+
+      LanItems.SetModified
       true
     end
 
