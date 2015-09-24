4c7d58cb357bcb73edf732657418f6ac1bd14ef9
Michal Filka <mfilka@suse.cz>
2015-09-24 08:07:39 +0200

    Use cleaner way for setting default values.

--- src/modules/LanItems.rb
+++ src/modules/LanItems.rb
@@ -1133,9 +1133,9 @@ module Yast
         LanItems.Items[current] = { "ifcfg" => device }
       end
 
-      autoinstall_settings["start_immediately"] = settings["start_immediately"] || false
-      autoinstall_settings["strict_IP_check_timeout"] = settings["strict_IP_check_timeout"] || -1
-      autoinstall_settings["keep_install_network"] = settings["keep_install_network"] || false
+      autoinstall_settings["start_immediately"] = settings.fetch("start_immediately", false)
+      autoinstall_settings["strict_IP_check_timeout"] = settings.fetch("strict_IP_check_timeout", -1)
+      autoinstall_settings["keep_install_network"] = settings.fetch("keep_install_network", false)
 
       # settings == {} has special meaning 'Reset' used by AY
       SetModified() if !settings.empty?
