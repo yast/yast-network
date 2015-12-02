ceea56fbe36f1db2565e9a846105fefed47859ee
Michal Filka <mfilka@suse.cz>
2015-12-02 10:14:05 +0100

    Converted Host-twospaces from old to new testsuite.

--- test/host_test.rb
+++ test/host_test.rb
@@ -6,18 +6,37 @@ require "yast"
 
 Yast.import "Host"
 
-describe "Host#Export" do
+describe "Host" do
   subject(:host) { Yast::Host }
 
-  let(:etc_hosts) do
-    {
-      "127.0.0.1"  => ["localhost localhost.localdomain"],
-      "10.20.1.29" => ["beholder"]
-    }
+  describe "#Export" do
+    let(:etc_hosts) do
+      {
+        "127.0.0.1"  => ["localhost localhost.localdomain"],
+        "10.20.1.29" => ["beholder"]
+      }
+    end
+
+    it "Successfully exports stored mapping" do
+      allow(host).to receive(:hosts).and_return(etc_hosts)
+      expect(host.Export).to eql({ "hosts" => etc_hosts })
+    end
   end
 
-  it "Successfully exports stored mapping" do
-    allow(host).to receive(:hosts).and_return(etc_hosts)
-    expect(host.Export).to eql({ "hosts" => etc_hosts })
+  describe "#Update" do
+    let(:etc_hosts) do
+      {
+        "127.0.0.1" => ["localhost localhost.localdomain"],
+        "10.0.0.1"  => ["somehost.example.com  notice-two-spaces"]
+      }
+    end
+
+    it "doesn't drop records with two spaces" do
+      host.Import({ "hosts" => etc_hosts })
+      host.Update("", "newname", ["10.0.0.42"])
+
+      tested_ip = "10.0.0.1"
+      expect(host.hosts[tested_ip]).to eql etc_hosts[tested_ip]
+    end
   end
 end
