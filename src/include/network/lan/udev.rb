2e73f669d996e6705b5f0e0fdb7b75a06ae82209
Michal Filka <mfilka@suse.cz>
2016-08-09 13:58:45 +0200

    Updated documentation

--- src/include/network/lan/udev.rb
+++ src/include/network/lan/udev.rb
@@ -47,7 +47,9 @@ module Yast
 
     # Returns a value of the particular key in the rule
     #
-    # @return [string] value corresponding to the key or empty string
+    # @param rule [array] an udev rule represented as a list of strings
+    # @param key  [string] a key name which is asked for value
+    # @return     [string] value corresponding to the key or empty string
     def udev_key_value(rule, key)
       raise ArgumentError, "Rule must not be nil when querying a key value" if rule.nil?
 
