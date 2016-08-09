85810348ca379d81b8f5696dc47e13100b6f9e0c
Michal Filka <mfilka@suse.cz>
2016-08-09 17:04:29 +0200

    Improved testsuite

--- test/hardware_test.rb
+++ test/hardware_test.rb
@@ -4,7 +4,9 @@ require_relative "test_helper"
 
 require "yast"
 
-class HardwareTestClass
+Yast.import "NetworkInterfaces"
+
+class HardwareTestClass < Yast::Module
   def initialize
     Yast.include self, "network/lan/hardware.rb"
   end
@@ -16,14 +18,10 @@ describe "#validate_hw" do
   let(:valid_name) { "eth0" }
   let(:long_name) { "verylongcustomnicname123" }
 
-  before(:each) do
-    allow(Yast::UI).to receive(:SetFocus)
-    expect(subject).to receive(:UsedNicName).and_return false
-  end
-
   it "passes for valid names only" do
     allow(subject).to receive(:devname_from_hw_dialog).and_return valid_name
 
+    expect(subject).to receive(:UsedNicName).and_return false
     expect(subject.validate_hw(nil, nil)).to be true
   end
 
@@ -31,6 +29,16 @@ describe "#validate_hw" do
   it "fails for long names" do
     allow(subject).to receive(:devname_from_hw_dialog).and_return long_name
 
+    expect(Yast::UI).to receive(:SetFocus)
+    expect(subject).to receive(:UsedNicName).and_return false
+    expect(subject.validate_hw(nil, nil)).to be false
+  end
+
+  it "fails for already used names" do
+    allow(subject).to receive(:devname_from_hw_dialog).and_return valid_name
+    allow(Yast::NetworkInterfaces).to receive(:List).and_return [valid_name]
+
+    expect(Yast::UI).to receive(:SetFocus)
     expect(subject.validate_hw(nil, nil)).to be false
   end
 end
