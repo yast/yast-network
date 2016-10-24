6e3ff2dc0fa52d9b12a229d75bc92779aef61402
Michal Filka <mfilka@suse.cz>
2016-10-24 09:36:43 +0200

    Added testsuite for valid_netmask?

--- test/routing_helpers_test.rb
+++ test/routing_helpers_test.rb
@@ -42,4 +42,40 @@ describe "RoutingHelpers" do
       expect(routing.convert_route_conf(obsolete_route_conf)).to eql correct_route_conf
     end
   end
+
+  describe "#valid_netmask?" do
+    it "accepts IPv4 netmask" do
+      expect(routing.valid_netmask?("255.0.0.0")).to be true
+    end
+
+    it "accepts IPv4 prefix" do
+      expect(routing.valid_netmask?("/24")).to be true
+    end
+
+    it "accepts IPv6 prefix" do
+      expect(routing.valid_netmask?("/128")).to be true
+    end
+
+    it "declines IPv6 netmask" do
+      expect(routing.valid_netmask?("fe00::")).to be false
+    end
+
+    it "declines nil or empty input" do
+      expect(routing.valid_netmask?("")).to be false
+      expect(routing.valid_netmask?(nil)).to be false
+    end
+
+    it "declines malformed prefix" do
+      expect(routing.valid_netmask?("/255/")).to be false
+      expect(routing.valid_netmask?("/255")).to be false
+      expect(routing.valid_netmask?("/-255")).to be false
+      expect(routing.valid_netmask?("/0")).to be false
+    end
+
+    it "declines malformed IPv4 netmask" do
+      expect(routing.valid_netmask?("/255.0.0.0")).to be false
+      expect(routing.valid_netmask?("255.0.255.0")).to be false
+      expect(routing.valid_netmask?("0.0.0.0")).to be false
+    end
+  end
 end
