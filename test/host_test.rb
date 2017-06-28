d7da55625271efd6ebf5a6c12f6bf48d29732b09
Martin Vidner <martin@vidner.net>
2017-06-28 13:12:05 +0200

    Removed a duplicate+corrupted test
    It is a corruption of the test above it.
    Revealed by a RSpec warning:
      WARNING: An expectation of `:Update` was set on `nil`.

--- test/host_test.rb
+++ test/host_test.rb
@@ -276,16 +276,6 @@ describe Yast::Host do
 
       Yast::Host.ResolveHostnameToStaticIPs
     end
-
-    it "doesn't call .Update when an IP already has a hostname" do
-      hostname = "linux"
-
-      Yast::Host.Update(hostname, hostname, static_ips[0])
-
-      expect(Yast::Host.ResolveHostnameToStaticIPs)
-        .not_to receive(:Update)
-        .with(fqhostname, fqhostname, static_ips[0])
-    end
   end
 
   describe ".StaticIPs" do
