672c8ffbb6aff2ffecf7d30d569156b122ff5e63
Knut Alejandro Anderssen González <teclator@gmail.com>
2017-09-25 11:06:16 +0100

    Load dhcp_set_hostname default from the control file.

--- src/lib/network/clients/inst_setup_dhcp.rb
+++ src/lib/network/clients/inst_setup_dhcp.rb
@@ -5,8 +5,15 @@ module Yast
     include Singleton
     include Logger
 
+    def initialize
+      Yast.import "Linuxrc"
+      Yast.import "DNS"
+    end
+
     def main
-      nac = Yast::NetworkAutoconfiguration.instance
+      nac = NetworkAutoconfiguration.instance
+      set_dhcp_hostname! if set_dhcp_hostname? && Stage.initial
+
       if !nac.any_iface_active?
         nac.configure_dhcp
       else
@@ -16,6 +23,25 @@ module Yast
       # if this is not wrapped in a def, ruby -cw says
       # warning: possibly useless use of a literal in void context
       :next
+    end
+
+    def set_dhcp_hostname?
+      set_hostname = Linuxrc.InstallInf("SetHostname")
+      log.info("SetHostname: #{set_hostname}")
+      set_hostname != 0
+    end
+
+    def set_dhcp_hostname!
+      DNS.dhcp_hostname = DNS.default_dhcp_hostname
+      log.info("Write dhcp hostname default: #{DNS.dhcp_hostname}")
+      SCR.Write(
+        Yast::Path.new(".sysconfig.network.dhcp.DHCLIENT_SET_HOSTNAME"),
+        DNS.dhcp_hostname ? "yes" : "no"
+      )
+      SCR.Write(
+        Yast::Path.new(".sysconfig.network.dhcp"),
+        nil
+      )
     end
   end
 end
