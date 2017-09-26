8867b67063a2a5496eceaa4fa2fee16acdfb3d65
Knut Alejandro Anderssen González <teclator@gmail.com>
2017-09-26 12:29:53 +0100

    Take in account linuxrc and set it according.

--- src/lib/network/clients/inst_setup_dhcp.rb
+++ src/lib/network/clients/inst_setup_dhcp.rb
@@ -12,7 +12,7 @@ module Yast
 
     def main
       nac = NetworkAutoconfiguration.instance
-      set_dhcp_hostname! if set_dhcp_hostname? && Stage.initial
+      set_dhcp_hostname! if Stage.initial
 
       if !nac.any_iface_active?
         nac.configure_dhcp
@@ -25,23 +25,38 @@ module Yast
       :next
     end
 
+    # Check if set of DHCLIENT_SET_HOSTNAME in /etc/sysconfig/network/dhcp has
+    # been disable by linuxrc cmdline
     def set_dhcp_hostname?
       set_hostname = Linuxrc.InstallInf("SetHostname")
       log.info("SetHostname: #{set_hostname}")
-      set_hostname != 0
+      set_hostname != "0"
     end
 
+    # Write the DHCLIENT_SET_HOSTNAME in /etc/sysconfig/network/dhcp based on
+    # linuxrc sethostname cmdline option if provided or the default value
+    # defined in the control file if not.
     def set_dhcp_hostname!
-      DNS.dhcp_hostname = DNS.default_dhcp_hostname
+      DNS.dhcp_hostname =
+        set_hostname_used? ? set_dhcp_hostname? : DNS.default_dhcp_hostname
+
       log.info("Write dhcp hostname default: #{DNS.dhcp_hostname}")
       SCR.Write(
         Yast::Path.new(".sysconfig.network.dhcp.DHCLIENT_SET_HOSTNAME"),
         DNS.dhcp_hostname ? "yes" : "no"
       )
+      # Flush cache
       SCR.Write(
         Yast::Path.new(".sysconfig.network.dhcp"),
         nil
       )
+    end
+
+    # Check whether the linuxrc sethostname option has been used or not
+    #
+    # @return [Boolean] true if sethosname linuxrc cmdline option was given
+    def set_hostname_used?
+      Linuxrc.InstallInf("SetHostnameUsed") == "1"
     end
   end
 end
