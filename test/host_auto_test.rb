038041664f7b7c6c887941f417edd5281518f1cd
Stefan Schubert <schubi@suse.de>
2018-06-22 14:15:54 +0200

    rubocop

--- test/host_auto_test.rb
+++ test/host_auto_test.rb
@@ -8,7 +8,7 @@ require_relative "../src/clients/host_auto"
 describe Yast::HostAutoClient do
   describe "#main" do
     before do
-      allow(Yast::WFM).to receive(:Args).with(no_args).and_return([func,hosts])
+      allow(Yast::WFM).to receive(:Args).with(no_args).and_return([func, hosts])
       allow(Yast::WFM).to receive(:Args).with(0).and_return(func)
       allow(Yast::WFM).to receive(:Args).with(1).and_return(hosts)
     end
@@ -16,7 +16,7 @@ describe Yast::HostAutoClient do
     context "when func is Import" do
       let(:func) { "Import" }
       let(:i_list) { double("IssuesList", add: nil) }
-      let(:hosts) { {"hosts" =>[{"host_address" => "10.20.1.29", "names" => [" "] }]} }
+      let(:hosts) { { "hosts" =>[{ "host_address" => "10.20.1.29", "names" => [" "] }] } }
 
       it "blames empty host name entries" do
         expect(Yast::AutoInstall).to receive(:issues_list).and_return(i_list)
