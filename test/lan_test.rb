b79a89b4a85399c6fefdf5a9f0724b6b371eef26
Michal Filka <mfilka@suse.cz>
2018-10-24 10:08:13 +0200

    Updated testsuite

--- test/lan_test.rb
+++ test/lan_test.rb
@@ -8,9 +8,9 @@ Yast.import "Lan"
 
 describe "LanClass#Packages" do
   packages = {
-    "iw"     => "wlan",
-    "vlan"   => "vlan",
-    "tunctl" => "tun"
+    "wpa_supplicant" => "wlan",
+    "vlan"           => "vlan",
+    "tunctl"         => "tun"
   }
 
   packages.each do |pkg, type|
@@ -36,29 +36,6 @@ describe "LanClass#Packages" do
         .and_return(false)
       expect(Yast::Lan.Packages).to include pkg
     end
-  end
-
-  it "lists wpa_supplicant package when WIRELESS_AUTH_MODE is psk or eap" do
-    allow(Yast::NetworkInterfaces)
-      .to receive(:List)
-      .and_return([])
-    allow(Yast::NetworkService)
-      .to receive(:is_network_manager)
-      .and_return(false)
-
-    # when checking options, LanClass#Packages currently cares only if
-    # WIRELESS_AUTH_MODE={psk, eap} is present
-    expect(Yast::NetworkInterfaces)
-      .to receive(:Locate)
-      .with("WIRELESS_AUTH_MODE", /(psk|eap)/)
-      .at_least(:once)
-      .and_return(["place_holder"])
-    expect(Yast::PackageSystem)
-      .to receive(:Installed)
-      .with("wpa_supplicant")
-      .at_least(:once)
-      .and_return(false)
-    expect(Yast::Lan.Packages).to include "wpa_supplicant"
   end
 
   it "lists NetworkManager package when NetworkManager service is selected" do
