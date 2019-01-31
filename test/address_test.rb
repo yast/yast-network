e0dbac2bf051ad224187869b963b065ef2e8770c
Michal Filka <mfilka@suse.cz>
2019-01-31 09:49:30 +0100

    Updated testsuite

--- test/address_test.rb
+++ test/address_test.rb
@@ -106,35 +106,39 @@ describe "NetworkLanAddressInclude" do
         .and_return(["#{initial_hostname} custom-name"])
     end
 
-    it "drops old /etc/hosts record if hostname was changed" do
-      expect(Yast::Host)
-        .to receive(:remove_ip)
-        .with(ip)
-      expect(Yast::Host)
-        .to receive(:Update)
-        .with(initial_hostname, new_hostname, ip)
+    context "when ip has not changed" do
+      it "drops old /etc/hosts record if hostname was changed" do
+        expect(Yast::Host)
+          .to receive(:remove_ip)
+          .with(ip)
+        expect(Yast::Host)
+          .to receive(:Update)
+          .with(initial_hostname, new_hostname, ip)
 
-      subject.send(:update_hostname, ip, new_hostname)
+        subject.send(:update_hostname, ip, new_hostname)
+      end
     end
 
-    it "keeps names untouched when only the ip was changed" do
-      new_ip = "2.2.2.2"
+    context "when ip has changed" do
+      it "keeps names if there is no change in hostname" do
+        new_ip = "2.2.2.2"
 
-      original_names = Yast::Host.names(ip)
-      subject.send(:update_hostname, new_ip, initial_hostname)
+        original_names = Yast::Host.names(ip)
+        subject.send(:update_hostname, new_ip, initial_hostname)
 
-      expect(Yast::Host.names(new_ip)).to eql original_names
-    end
+        expect(Yast::Host.names(new_ip)).to eql original_names
+      end
 
-    it "does not crash when ip was changed, no hostnames exist and the hostname is empty" do
-      new_ip = "2.2.2.2"
+      it "does not crash when no hostnames exist for old ip and new hostname is not set" do
+        new_ip = "2.2.2.2"
 
-      # targeted especially against newly created devices ;-)
-      allow(Yast::LanItems)
-        .to receive(:ipaddr)
-        .and_return("")
+        # targeted especially against newly created devices ;-)
+        allow(Yast::LanItems)
+          .to receive(:ipaddr)
+          .and_return("")
 
-      expect { subject.send(:update_hostname, new_ip, "") }.not_to raise_error
+        expect { subject.send(:update_hostname, new_ip, "") }.not_to raise_error
+      end
     end
   end
 
