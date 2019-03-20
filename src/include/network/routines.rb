48b80dd3a101571392052db918a89f10a49c69d5
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-03-20 20:28:22 +0000

    Do not check the driver but if it start with 'virtio'

--- src/include/network/routines.rb
+++ src/include/network/routines.rb
@@ -625,7 +625,9 @@ module Yast
 
           one["bus"] = bus
           one["busid"] = card["sysfs_bus_id"] || ""
-          one["parent_busid"] = one["sysfs_id"].split("/")[-2] if one["driver"] == "virtio_net"
+          if one["busid"].start_with?("virtio")
+            one["parent_busid"] = one["sysfs_id"].split("/")[-2]
+          end
           one["mac"] = Ops.get_string(resource, ["hwaddr", 0, "addr"], "")
           # is the cable plugged in? nil = don't know
           one["link"] = Ops.get(resource, ["link", 0, "state"])
