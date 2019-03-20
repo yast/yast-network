2a6a9110d56b271d4747fb4d4cdcf662c8aa49d2
Imobach González Sosa <igonzalezsosa@suse.com>
2019-03-20 11:58:26 +0000

    Remove (wrong) default value from ConfigReader.for method

--- src/lib/y2network/config_reader.rb
+++ src/lib/y2network/config_reader.rb
@@ -19,10 +19,14 @@
 module Y2Network
   # This module contains a set of classes to read the network configuration from the system
   #
-  # For the time being, only the wicked reader ({Y2Network::ConfigReader::Wicked}) reader is
+  # For the time being, only the wicked reader ({Y2Network::ConfigReader::Sysconfig}) reader is
   # available.
   module ConfigReader
-    def self.for(source = :wicked)
+    # Config reader for a given source
+    #
+    # @param source [Symbol] Source name (e.g., :sysconfig)
+    # @return [#config] Configuration reader from {Y2Network::ConfigReader}
+    def self.for(source)
       require "y2network/config_reader/#{source}"
       name = source.to_s.split("_").map(&:capitalize).join
       klass = const_get(name)
