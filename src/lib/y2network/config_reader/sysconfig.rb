2afa4f7db80fe7c1fbd549c47a7a9981c3610b41
Michal Filka <mfilka@suse.cz>
2019-03-24 16:23:13 +0100

    Implemented RoutesReader

--- src/lib/y2network/config_reader/sysconfig.rb
+++ src/lib/y2network/config_reader/sysconfig.rb
@@ -21,6 +21,7 @@ require "y2network/interface"
 require "y2network/routing"
 require "y2network/routing_table"
 require "y2network/route"
+require "y2network/config_reader/routes_reader"
 
 Yast.import "NetworkInterfaces"
 Yast.import "Routing"
@@ -32,14 +33,13 @@ module Y2Network
       # @return [Y2Network::Config] Network configuration
       def config
         interfaces = find_interfaces
-        routing = Y2Network::Routing.new(tables: find_routing_tables(interfaces))
+        # load /etc/sysconfig/network/routes
+        routing = Y2Network::Routing.new(tables: [RoutesReader.new.config])
+        # TODO: RoutesReader(s) for ifroute-* files
         Config.new(interfaces: interfaces, routing: routing)
       end
 
     private
-
-      MISSING_VALUE = "-".freeze
-      private_constant :MISSING_VALUE
 
       # Find configured network interfaces
       #
@@ -51,46 +51,6 @@ module Y2Network
         Yast::NetworkInterfaces.List("").map do |name|
           Y2Network::Interface.new(name)
         end
-      end
-
-      # Find routing tables
-      #
-      # @note For the time being, only one routing table is considered.
-      #
-      # @param interfaces [Array<Interface>] Detected interfaces
-      # @return [Array<RoutingTable>]
-      #
-      # @see Yast::Routing.Routes
-      def find_routing_tables(interfaces)
-        Yast::Routing.Read
-        routes = Yast::Routing.Routes.map { |h| build_route(interfaces, h) }
-        table = Y2Network::RoutingTable.new(routes)
-        [table]
-      end
-
-      # Build a route given a hash from the SCR agent
-      #
-      # @param interfaces [Array<Interface>] List of detected interfaces
-      # @param hash [Hash] Hash from the `.routes` SCR agent
-      # @return Route
-      def build_route(interfaces, hash)
-        iface = interfaces.find { |i| i.name == hash["device"] }
-        Y2Network::Route.new(
-          to:        build_ip(hash["destination"], hash["netmask"]) || :default,
-          interface: iface,
-          gateway:   build_ip(hash["gateway"], MISSING_VALUE)
-        )
-      end
-
-      # Given an IP and a netmaks, returns a valid IPAddr objecto
-      #
-      # @param ip_str      [String] IP address; {MISSING_VALUE} means that the IP is not defined
-      # @param netmask_str [String] Netmask; {MISSING_VALUE} means than no netmaks was specified
-      # @return [IPAddr,nil] The IP address or `nil` if the IP is missing
-      def build_ip(ip_str, netmask_str = nil)
-        return nil if ip_str == MISSING_VALUE
-        ip = IPAddr.new(ip_str)
-        netmask_str == MISSING_VALUE ? ip : ip.mask(netmask_str)
       end
     end
   end
