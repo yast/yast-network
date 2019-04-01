922f4c19bef900382a31bab6a5e9adca213f8194
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-04-01 10:25:15 +0100

    Ip forwarding initialization & small fixes

--- src/lib/y2network/routing.rb
+++ src/lib/y2network/routing.rb
@@ -16,6 +16,7 @@
 #
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
+
 module Y2Network
   # General routing configuration storage (routing tables, forwarding setup, ...)
   class Routing
