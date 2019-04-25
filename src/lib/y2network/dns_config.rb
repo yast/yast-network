bb36588cc9914447c626cad7698739e8890b1d84
Imobach González Sosa <igonzalezsosa@suse.com>
2019-04-25 15:54:06 +0100

    Add a basic DNSConfig class

---
+++ src/lib/y2network/dns_config.rb
@@ -0,0 +1,59 @@
+# Copyright (c) [2019] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+module Y2Network
+  # DNS configuration (hostname, name servers, etc.).
+  class DNSConfig
+    # @return [String] Hostname (local part)
+    attr_reader :hostname
+
+    # @return [Array<IPAddr>] List of name servers
+    attr_reader :name_servers
+
+    # @return [Array<String>] List of search domains
+    attr_reader :search_domains
+
+    # @return [String] resolv.conf update policy
+    attr_reader :resolv_conf_policy
+
+    # @return [Boolean] Whether to write the hostname to /etc/hosts
+    attr_reader :hostname_to_hosts
+
+    # @return [Boolean] Whether to take the hostname from DHCP
+    attr_reader :dhcp_hostname
+
+    # @todo receive an array instead all these arguments
+    #
+    # @param opts [Hash] DNS configuration options
+    # @option opts [String] :hostname
+    # @option opts [Array<String>] :name_servers
+    # @option opts [Array<String>] :search_domains
+    # @option opts [ResolvConfPolicy] :resolv_conf_policy
+    # @option opts [Boolean] :hostname_to_hosts
+    # @option opts [Boolean] :dhcp_hostname
+    def initialize(opts = {})
+      @hostname = opts[:hostname]
+      @name_servers = opts[:name_servers] || []
+      @search_domains = opts[:search_domains] || []
+      @resolv_conf_policy = opts[:resolv_conf_policy]
+      @hostname_to_hosts = opts[:hostname_to_hosts]
+      @dhcp_hostname = opts[:dhcp_hostname]
+    end
+  end
+end
---
+++ test/y2network/config_reader/sysconfig_dns_test.rb
@@ -0,0 +1,148 @@
+# Copyright (c) [2019] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+require_relative "../../test_helper"
+require "y2network/config_reader/sysconfig_dns"
+
+describe Y2Network::ConfigReader::SysconfigDNS do
+  subject(:reader) { described_class.new }
+
+  describe "#config" do
+    let(:netconfig_dns) do
+      {
+        "NETCONFIG_DNS_POLICY"            => "auto",
+        "NETCONFIG_DNS_STATIC_SERVERS"    => "1.1.1.1 2.2.2.2",
+        "NETCONFIG_DNS_STATIC_SEARCHLIST" => "example.net mydomain"
+      }
+    end
+
+    let(:netconfig_dhcp) do
+      {
+        "DHCLIENT_SET_HOSTNAME"   => "yes",
+        "WRITE_HOSTNAME_TO_HOSTS" => "yes"
+      }
+    end
+
+    let(:netconfig) do
+      { "config" => netconfig_dns, "dhcp" => netconfig_dhcp }
+    end
+
+    let(:executor) do
+      double("Yast::Execute", on_target!: "")
+    end
+
+    before do
+      allow(Yast::SCR).to receive(:Read) do |arg|
+        key_parts = arg.to_s.split(".")
+        netconfig.dig(*key_parts[3..-1])
+      end
+      allow(Yast::Execute).to receive(:stdout).and_return(executor)
+    end
+
+    it "returns the DNS configuration" do
+      config = reader.config
+      expect(config).to be_a(Y2Network::DNSConfig)
+    end
+
+    it "includes the hostname" do
+      expect(executor).to receive(:on_target!).with(/hostname/).and_return("foo")
+      config = reader.config
+      expect(config.hostname).to eq("foo")
+    end
+
+    it "includes the list of name servers" do
+      config = reader.config
+      expect(config.name_servers).to eq([IPAddr.new("1.1.1.1"), IPAddr.new("2.2.2.2")])
+    end
+
+    context "when no name servers are defined" do
+      let(:netconfig_dns) do
+        { "NETCONFIG_DNS_STATIC_SERVERS" => "" }
+      end
+
+      it "includes an empty list of name servers" do
+        config = reader.config
+        expect(config.name_servers).to eq([])
+      end
+    end
+
+    it "includes a list of domains to search" do
+      config = reader.config
+      expect(config.search_domains).to eq(["example.net", "mydomain"])
+    end
+
+    context "when no list of domains to search is defined" do
+      let(:netconfig_dns) do
+        { "NETCONFIG_DNS_STATIC_SEARCHLIST" => "" }
+      end
+
+      it "includes an empty list of domains to search" do
+        config = reader.config
+        expect(config.search_domains).to eq([])
+      end
+    end
+
+    it "sets the DNS policy" do
+      config = reader.config
+      expect(config.resolv_conf_policy).to eq("auto")
+    end
+
+    context "when no DNS policy is specified" do
+      let(:netconfig_dns) do
+        { "NETCONFIG_DNS_POLICY" => "" }
+      end
+
+      it "sets the policy to the 'default' value" do
+        config = reader.config
+        expect(config.resolv_conf_policy).to eq("default")
+      end
+    end
+
+    it "sets the hostname_to_hosts parameter" do
+      config = reader.config
+      expect(config.hostname_to_hosts).to eq(true)
+    end
+
+    context "when WRITE_HOSTNAME_TO_HOSTS is not set to 'yes'" do
+      let(:netconfig_dhcp) do
+        { "WRITE_HOSTNAME_TO_HOSTS" => "no" }
+      end
+
+      it "sets hostname_to_hosts to false" do
+        config = reader.config
+        expect(config.hostname_to_hosts).to eq(false)
+      end
+    end
+
+    it "sets the dhcp_hostname parameter" do
+      config = reader.config
+      expect(config.hostname_to_hosts).to eq(true)
+    end
+
+    context "when DHCLIENT_SET_HOSTNAME is not set to 'yes'" do
+      let(:netconfig_dhcp) do
+        { "DHCLIENT_SET_HOSTNAME" => "no" }
+      end
+
+      it "sets dhcp_hostname to false" do
+        config = reader.config
+        expect(config.hostname_to_hosts).to eq(false)
+      end
+    end
+  end
+end
