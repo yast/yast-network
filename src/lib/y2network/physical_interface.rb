b5c1fe5e0272e49ba6f87d6fd941490a794ec4ba
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-05-16 08:56:00 +0100

    Remove duplicated hw class

--- src/lib/y2network/physical_interface.rb
+++ src/lib/y2network/physical_interface.rb
@@ -2,8 +2,10 @@ require "y2network/interface"
 require "y2network/hardware_info"
 
 module Y2Network
+  # This class represents a physical interface (ethernet, wireless,
+  # infiniband...)
   class PhysicalInterface < Interface
-    # @return [HardwareInfo]
+    # @return [Hwinfo]
     attr_accessor :hwinfo
   end
 end
--- src/lib/y2network/virtual_interface.rb
+++ src/lib/y2network/virtual_interface.rb
@@ -1,6 +1,8 @@
 require "y2network/interface"
 
 module Y2Network
+  # This class represents a virtual interface (dummy, vlan, bond,
+  # bridge, veth ...)
   class VirtualInterface < Interface
   end
 end
