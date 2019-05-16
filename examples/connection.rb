42fc1c0efaf7234eb8c8c23f0d509a49bc27ba0a
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-05-16 12:48:38 +0100

    Update connection example based on CR.

--- examples/connection.rb
+++ examples/connection.rb
@@ -9,19 +9,19 @@ require "y2network/connection_config/wireless"
 
 config = Y2Network::Config.from(:sysconfig)
 
-eth0 = config.interfaces.find("eth0").first
+eth0 = config.interfaces.find { |i| i.name == "eth0" }
 eth_conn = Y2Network::ConnectionConfig::Ethernet.new
 eth_conn.interface = eth0
 
-eth1 = config.interfaces.find("eth1").first
-eth2 = config.interfaces.find("eth2").first
+eth1 = config.interfaces.find { |i| i.name == "eth1" }
+eth2 = config.interfaces.find { |i| i.name == "eth2" }
 bond = Y2Network::ConnectionConfig::Bond.new
 bond.slaves = [eth1, eth2]
 bond.options = "mode=active-backup miimon=100"
 bond.interface = Y2Network::VirtualInterface.new("bond0")
 config.interfaces << bond
 
-wlan0 = config.interfaces.find("wlan0").first
+wlan0 = config.interfaces.find {|i| i.name == "wlan0" }
 wlan_conn = Y2Network::ConnectionConfig::Wireless.new
 wlan_conn.essid = "TEST_WIFI"
 wlan_conn.interface = wlan0
