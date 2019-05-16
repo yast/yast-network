d908ff4527baaa81b8f934a34759d384de23f596
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-05-16 08:09:05 +0100

    Remove Interfaces namespace

--- src/lib/y2network/physical_interface.rb
+++ src/lib/y2network/physical_interface.rb
@@ -2,10 +2,8 @@ require "y2network/interface"
 require "y2network/hardware_info"
 
 module Y2Network
-  module Interfaces
-    class PhysicalInterface < Interface
-      # @return [HardwareInfo]
-      attr_accessor :hwinfo
-    end
+  class PhysicalInterface < Interface
+    # @return [HardwareInfo]
+    attr_accessor :hwinfo
   end
 end
