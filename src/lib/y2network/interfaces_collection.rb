350ea0f4bb35e733c147247ca322b20ac2eeed8f
Imobach González Sosa <igonzalezsosa@suse.com>
2019-05-16 11:16:34 +0100

    Add an InterfacesCollection class
    * Taken from https://github.com/yast/yast-network/pull/796.

---
+++ src/lib/y2network/interfaces_collection.rb
@@ -0,0 +1,73 @@
+# Copyright (c) [2019] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require "y2network/interface"
+require "forwardable"
+
+module Y2Network
+  # A container for network devices. In the end should implement methods for mass operations over
+  # network interfaces like old LanItems::find_dhcp_ifaces.
+  #
+  # @example Create a new collection
+  #   eth0 = Y2Network::Interface.new("eth0")
+  #   collection = Y2Network::InterfacesCollection.new(eth0)
+  #
+  # @example Find an interface using its name
+  #   iface = collection.find("eth0") #=> #<Y2Network::Interface:0x...>
+  class InterfacesCollection
+    # FIXME: Direct access to be replaced to make possible
+    # Y2Network::Config.interfaces.eth0
+    # Y2Network::Config.interfaces.of_type(:eth)
+    # ...
+    attr_reader :interfaces
+
+    extend Forwardable
+
+    def_delegators :@interfaces, :each, :map, :select, :push
+
+    # Constructor
+    #
+    # @param interfaces [Array<Interface>] List of interfaces
+    def initialize(interfaces)
+      @interfaces = interfaces
+    end
+
+    # @param name [String] Returns the interface with the given name
+    def find(name)
+      interfaces.find { |i| !i.name.nil? ? i.name == name : i.hardware.name }
+    end
+
+    # Add an interface with the given name
+    #
+    # @param name [String] Interface's name
+    def add(name)
+      interfaces.push(Interface.new(name))
+    end
+
+    # Compares InterfacesCollections
+    #
+    # @return [Boolean] true when both collections contain only equal interfaces,
+    #                   false otherwise
+    def ==(other)
+      @interfaces - other.interfaces && other.interfaces == @interfaces
+    end
+
+    alias_method :eql?, :==
+  end
+end
---
+++ test/y2network/interfaces_collection_test.rb
@@ -0,0 +1,52 @@
+# Copyright (c) [2019] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../test_helper"
+require "y2network/interfaces_collection"
+
+describe Y2Network::InterfacesCollection do
+  subject(:collection) { described_class.new(interfaces) }
+
+  let(:eth0) { Y2Network::Interface.new("eth0") }
+  let(:wlan0) { Y2Network::Interface.new("wlan0") }
+  let(:interfaces) { [eth0, wlan0] }
+
+  describe "#find" do
+    it "returns the interface with the given name" do
+      expect(collection.find("eth0")).to eq(eth0)
+    end
+  end
+
+  describe "#add" do
+    it "adds an interface with the given name" do
+      collection.add("eth1")
+      eth1 = collection.find("eth1")
+      expect(eth1.name).to eq("eth1")
+    end
+  end
+
+  describe "#push" do
+    let(:wlan1) { Y2Network::Interface.new("wlan1") }
+
+    it "adds an interface to the list" do
+      collection.push(wlan1)
+      expect(collection.find(wlan1.name)).to eq(wlan1)
+    end
+  end
+end
