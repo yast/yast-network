3ba1db5852d50a11e6e53616e12f43c874499643
Josef Reidinger <jreidinger@suse.cz>
2019-05-21 08:49:55 +0200

    adapt to new workflow

---
+++ src/lib/y2network/dialogs/add_interface.rb
@@ -0,0 +1,51 @@
+require "cwm/dialog"
+require "y2network/widgets/interface_type"
+
+Yast.import "Label"
+Yast.import "LanItems"
+Yast.import "NetworkInterfaces"
+
+module Y2Network
+  module Dialogs
+    # Dialog to create or edit route.
+    class AddInterface < CWM::Dialog
+      def initialize
+        @type_widget = Widgets::InterfaceType.new
+      end
+
+      def contents
+        HBox(
+          @type_widget
+        )
+      end
+
+      def run
+        ret = super
+        ret = :cancel if ret == :abort
+        # TODO: replace with builder initialization
+        Yast::LanItems.type = @type_widget.result
+        proposed_name = Yast::LanItems.new_type_devices(@type_widget.result, 1).first
+        Yast::LanItems.device = proposed_name
+        Yast::NetworkInterfaces.Name = proposed_name
+        Yast::LanItems.Items[Yast::LanItems.current]["ifcfg"] = proposed_name
+        Yast::LanItems.Items[Yast::LanItems.current]["udev"] = {}
+        # END of TODO
+
+        ret
+      end
+
+      def back_button
+        ""
+      end
+
+      def abort_button
+        Yast::Label.CancelButton
+      end
+
+      # always open new dialog
+      def should_open_dialog?
+        true
+      end
+    end
+  end
+end
