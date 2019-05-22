4f47c1c2e927e2f2685fe8df1803546a04f79b0a
Josef Reidinger <jreidinger@suse.cz>
2019-05-22 10:35:00 +0200

    adapt tests

---
+++ test/y2network/widgets/interface_name_test.rb
@@ -0,0 +1,59 @@
+# Copyright (c) [2019] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../test_helper"
+require "cwm/rspec"
+
+require "y2network/widgets/interface_name"
+
+describe Y2Network::Widgets::InterfaceName do
+  subject { described_class.new({"IFCFGTYPE" => "eth"}) }
+
+  include_examples "CWM::ComboBox"
+
+  describe "#validate" do
+    let(:valid_name) { "eth0" }
+    let(:long_name) { "verylongcustomnicname123" }
+
+    it "passes for valid names only" do
+      allow(subject).to receive(:value).and_return valid_name
+
+      expect(Yast::NetworkInterfaces).to receive(:List).and_return([])
+      expect(subject.validate).to be true
+    end
+
+    # bnc#991486
+    it "fails for long names" do
+      allow(subject).to receive(:value).and_return long_name
+
+      expect(Yast::NetworkInterfaces).to receive(:List).and_return([])
+      expect(Yast::UI).to receive(:SetFocus)
+      expect(subject.validate).to be false
+    end
+
+    it "fails for already used names" do
+      allow(subject).to receive(:value).and_return valid_name
+      allow(Yast::NetworkInterfaces).to receive(:List).and_return [valid_name]
+
+      expect(Yast::UI).to receive(:SetFocus)
+      expect(subject.validate).to be false
+    end
+  end
+end
+
