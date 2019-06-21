7632cb4cedcb2fca17c1975d5d3efb1e30ef60eb
Michal Filka <mfilka@suse.cz>
2019-06-21 10:12:01 +0200

    Fixed incorrectly used GetBondableInterfaces in the bridge widget init

--- src/lib/y2network/widgets/bridge_ports.rb
+++ src/lib/y2network/widgets/bridge_ports.rb
@@ -29,7 +29,7 @@ module Y2Network
       def init
         br_ports = @settings["BRIDGE_PORTS"].split
         items = slave_items_from(
-          @settings.bridgable_interfaces,
+          Yast::LanItems.GetBridgeableInterfaces(Yast::LanItems.GetCurrentName),
           br_ports
         )
 
