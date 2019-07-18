302c86c6799f6d17dc77abc4b318e97edbbc366a
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-07-18 10:23:31 +0100

    Fix connection example and added vlan connection

--- examples/connection.rb
+++ examples/connection.rb
@@ -3,7 +3,9 @@ require "y2network/config"
 require "y2network/virtual_interface"
 require "y2network/connection_config/ethernet"
 require "y2network/connection_config/bonding"
+require "y2network/connection_config/bridge"
 require "y2network/connection_config/wireless"
+require "y2network/connection_config/vlan"
 
 # Historically yast2-network has been tightly coupled to ifconfig or wicked
 # configuration. One of the goals of the new network-ng is to be backend
@@ -20,15 +22,21 @@ require "y2network/connection_config/wireless"
 # speed based etc...)
 #
 # Below we will show an example of how the new 'ConnectionConfig' could be used
-# to configure our network.
-
-# given an interfaces list (interfaces)
+# to configure this network topology assuming physical interfaces are part of
+# current interfaces config list:
+#
+#         ###########      ###############     ##############
+#         #  bond0  #------#  bond0.100  #-----#  virbr100  #
+#         ###########      ###############     ##############
+#              |
+#        -------------
+#       |             |
+#  ##########    ##########    ##########     ###########
+#  #  eth1  #    #  eth2  #    #  eth0  #     #  wlan0  #
+#  ##########    ##########    ##########     ###########
+#
 
 config = Y2Network::Config.from(:sysconfig)
-
-eth0 = config.interfaces.by_name("eth0")
-eth_conn = Y2Network::ConnectionConfig::Ethernet.new
-eth_conn.interface = eth0
 
 eth1 = config.interfaces.by_name("eth1")
 eth2 = config.interfaces.by_name("eth2")
@@ -37,6 +45,19 @@ bond.slaves = [eth1, eth2]
 bond.options = "mode=active-backup miimon=100"
 bond.interface = Y2Network::VirtualInterface.new("bond0")
 config.interfaces << bond.interface
+
+vlan100 = Y2Network::ConnectionConfig::Vlan.new
+vlan100.etherdevice = bond.interface
+vlan100.vlan_id = "100"
+vlan100.interface = Y2Network::VirtualInterface.new("bond0.100")
+
+virbr100 = Y2Network::ConnectionConfig::Bridge.new
+virbr100.ports = [vlan100.interface]
+virbr100.interface = Y2Network::VirtualInterface.new("virbr100")
+
+eth0 = config.interfaces.by_name("eth0")
+eth_conn = Y2Network::ConnectionConfig::Ethernet.new
+eth_conn.interface = eth0
 
 wlan0 = config.interfaces.by_name("wlan0")
 wlan_conn = Y2Network::ConnectionConfig::Wireless.new
