3ff75b888152c55b7c788c330711844770f0aa11
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-08-13 10:12:44 +0100

    Changes based on code review.

--- src/lib/y2network/sysconfig/connection_config_writers/usb.rb
+++ src/lib/y2network/sysconfig/connection_config_writers/usb.rb
@@ -17,14 +17,14 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
-require "y2network/sysconfig/connection_config_writers/ethernet"
+require "y2network/sysconfig/connection_config_writers/base"
 
 module Y2Network
   module Sysconfig
     module ConnectionConfigWriters
       # This class is responsible for writing the information from a ConnectionConfig::Usb
       # object to the underlying system.
-      class Usb < Ethernet
+      class Usb < Base
       end
     end
   end
