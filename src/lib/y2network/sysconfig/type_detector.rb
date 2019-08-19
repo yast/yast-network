d507d1765c7cf1bf161866de5fe27850bb949c96
Michal Filka <mfilka@suse.cz>
2019-08-19 15:29:30 +0200

    Refactoring in type detector / interface file

--- src/lib/y2network/sysconfig/type_detector.rb
+++ src/lib/y2network/sysconfig/type_detector.rb
@@ -20,8 +20,7 @@
 require "yast"
 require "y2network/interface_type"
 require "y2network/type_detector"
-
-Yast.import "NetworkInterfaces"
+require "y2network/sysconfig/interface_file"
 
 module Y2Network
   module Sysconfig
@@ -31,87 +30,12 @@ module Y2Network
       class << self
       private
 
-        KEY_TO_TYPE = {
-          "ETHERDEVICE"   => InterfaceType::VLAN,
-          "WIRELESS_MODE" => InterfaceType::WIRELESS,
-          "MODEM_DEVICE"  => InterfaceType::PPP
-        }.freeze
 
         # Checks wheter iface type can be recognized by interface configuration
         def type_by_config(iface)
-          devmap = devmap(iface)
-          return nil if !devmap
-
-          type_by_key_value(devmap) ||
-            type_by_key_existence(devmap) ||
-            type_from_interfacetype(devmap) ||
-            type_by_name(iface)
-        end
-
-        # Provides interface's sysconfig configuration
-        #
-        # @return [Hash{String => Object}] parsed configuration
-        def devmap(iface)
-          scr_path = ".network.value.\"#{iface}\""
-          values = Yast::SCR.Dir(Yast::Path.new(scr_path))
-
-          # provide configuration in canonicalized format
-          devmap = Yast::NetworkInterfaces.generate_config(scr_path, values)
-
-          log.info("TypeDetector: #{iface} configuration: #{devmap.inspect}")
-
-          devmap
-        end
-
-        # Detects interface type according to type specific option
-        #
-        # @param devmap [Hash<String, String>] a sysconfig configuration of an interface
-        #
-        # @return [Y2Network::InterfaceType, nil] particular type if recognized, nil otherwise
-        def type_by_key_existence(devmap)
-          key = KEY_TO_TYPE.keys.find { |k| devmap.include?(k) }
-          return KEY_TO_TYPE[key] if key
-
-          nil
-        end
-
-        # Detects interface type according to type specific option and its value
-        #
-        # @param devmap [Hash<String, String>] a sysconfig configuration of an interface
-        #
-        # @return [Y2Network::InterfaceType, nil] particular type if recognized, nil otherwise
-        def type_by_key_value(devmap)
-          return InterfaceType::BONDING if devmap["BONDING_MASTER"] == "yes"
-          return InterfaceType::BRIDGE if devmap["BRIDGE"] == "yes"
-          return InterfaceType::WIRELESS if devmap["WIRELESS"] == "yes"
-          return InterfaceType.from_short_name(devmap["TUNNEL"]) if devmap["TUNNEL"]
-
-          # in relation to original implementation ommited ENCAP option which leads to isdn
-          # and PPPMODE which leads to ppp. Neither of this type has been handled as
-          # "netcard" - see Yast::NetworkInterfaces for details
-
-          nil
-        end
-
-        # Detects interface type according to sysconfig's INTERFACETYPE option
-        #
-        # @param devmap [Hash<String, String>] a sysconfig configuration of an interface
-        #
-        # @return [Y2Network::InterfaceType, nil] type according to INTERFACETYPE option
-        #                                    value if recognized, nil otherwise
-        def type_from_interfacetype(devmap)
-          return InterfaceType.from_short_name(devmap["INTERFACETYPE"]) if devmap["INTERFACETYPE"]
-          nil
-        end
-
-        # Distinguishes interface type by its name
-        #
-        # The only case should be loopback device with special name (in sysconfig) "lo"
-        #
-        # @return [Y2Network::InterfaceType, nil] InterfaceType::LO or nil if not loopback
-        def type_by_name(iface)
-          InterfaceType::LO if iface == "lo"
-          nil
+          iface_file = Y2Network::Sysconfig::InterfaceFile.find(iface)
+          iface_file.load
+          iface_file.type
         end
       end
     end
