c9a2dce87e89a905c6965d2aae980ada4d500bf4
Imobach González Sosa <igonzalezsosa@suse.com>
2019-08-29 10:31:41 +0100

    Extend unit test coverage

--- test/y2network/widgets/udev_rules_test.rb
+++ test/y2network/widgets/udev_rules_test.rb
@@ -27,12 +27,62 @@ require "y2network/interface_config_builder"
 describe Y2Network::Widgets::UdevRules do
   subject { described_class.new(builder) }
 
-  let(:builder) { instance_double(Y2Network::InterfaceConfigBuilder) }
+  let(:builder) { instance_double(Y2Network::InterfaceConfigBuilder, name: "eth0") }
+  let(:dialog_ret) { :ok }
+  let(:dialog) { instance_double(Y2Network::Dialogs::RenameInterface, run: dialog_ret) }
 
   before do
     allow(Yast::LanItems).to receive(:current_udev_name).and_return("hell666")
-    allow(Y2Network::Dialogs::RenameInterface).to receive(:new).and_return(double(run: "heaven010"))
+    allow(Y2Network::Dialogs::RenameInterface).to receive(:new).and_return(dialog)
   end
 
   include_examples "CWM::CustomWidget"
+  include Yast::UIShortcuts
+
+  describe "#init" do
+    it "initializes the input field with the current name" do
+      expect(subject).to receive(:value=).with("eth0")
+      subject.init
+    end
+  end
+
+  describe "#handle" do
+    it "opens the rename interface dialog" do
+      expect(dialog).to receive(:run)
+      subject.handle
+    end
+
+    context "when the dialog returns :ok" do
+      it "updates the current value" do
+        expect(subject).to receive(:value=).with(builder.name)
+        subject.handle
+      end
+    end
+
+    context "when the dialog does not returns :ok" do
+      let(:dialog_ret) { :cancel }
+
+      it "does not update the current value" do
+        expect(subject).to_not receive(:value=)
+        subject.handle
+      end
+    end
+  end
+
+  describe "#value=" do
+    it "updates the input field with the given name" do
+      expect(Yast::UI).to receive(:ChangeWidget).with(Id(:udev_rules_name), :Value, "eth1")
+      subject.value = "eth1"
+    end
+  end
+
+  describe "#value" do
+    before do
+      expect(Yast::UI).to receive(:QueryWidget).with(Id(:udev_rules_name), :Value).and_return("eth1")
+    end
+
+    it "returns the value from the input field" do
+      expect(subject.value).to eq("eth1")
+    end
+  end
 end
