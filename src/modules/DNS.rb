5c68f14d342025efca321ba0b19e88a266c3e068
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-10-21 08:42:23 +0100

    Make rubocop happy

--- src/modules/DNS.rb
+++ src/modules/DNS.rb
@@ -192,8 +192,8 @@ module Yast
     def Write(_gui: true, netconfig_update: true)
       writer = Y2Network::Sysconfig::DNSWriter.new
       writer.write(Yast::Lan.yast_config.dns,
-                   Yast::Lan.system_config.dns,
-                   netconfig_update: netconfig_update)
+        Yast::Lan.system_config.dns,
+        netconfig_update: netconfig_update)
       true
     end
 
