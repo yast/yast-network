6d767dcfb55695c73e4203e059f06ff6a8a87b99
Knut Alejandro Anderssen González <teclator@gmail.com>
2019-10-31 12:33:35 +0000

    Changes based on CR.

--- src/modules/DNS.rb
+++ src/modules/DNS.rb
@@ -188,8 +188,10 @@ module Yast
     # Write new DNS and hostname settings
     # Includes Host,NetworkConfig::Write
     # @todo Update GUI
+    # @param netconfig_update [Boolean] Whether 'netconfig update' should be
+    #   called after writing the DNS configuration or not
     # @return true if success
-    def Write(_gui: true, netconfig_update: true)
+    def Write(netconfig_update: true)
       writer = Y2Network::Sysconfig::DNSWriter.new
       writer.write(Yast::Lan.yast_config.dns,
         Yast::Lan.system_config.dns,
