de567b436fd2b10e4ec445a47b2683a4529131d7
Stefan Schubert <schubi@suse.de>
2020-05-08 14:19:05 +0200

    fixed pathes

--- src/lib/y2network/autoinst_profile/routing_section.rb
+++ src/lib/y2network/autoinst_profile/routing_section.rb
@@ -17,7 +17,7 @@
 # To contact SUSE LLC about this file by physical or electronic mail, you may
 # find current contact information at www.suse.com.
 
-require "y2network/autoinst_profile/section_with_attributes"
+require "installation/autoinst_profile/section_with_attributes"
 require "y2network/autoinst_profile/route_section"
 
 module Y2Network
@@ -39,7 +39,7 @@ module Y2Network
     #  </routing>
     #
     # @see NetworkingSection
-    class RoutingSection < SectionWithAttributes
+    class RoutingSection < ::Installation::AutoinstProfile::SectionWithAttributes
       def self.attributes
         [
           { name: :ipv4_forward },
