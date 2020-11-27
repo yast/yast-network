bcf7811d0a402e41589f09d129f7acb13e337352
Knut Alejandro Anderssen González <teclator@gmail.com>
2020-11-27 08:48:55 +0000

    Show in a popup a table with all available networks

---
+++ src/lib/y2network/dialogs/wireless_networks.rb
@@ -0,0 +1,34 @@
+require "y2network/dialogs/popup"
+require "y2network/widgets/wireless_networks"
+
+module Y2Network
+  module Dialogs
+    class WirelessNetworks < Popup
+      def initialize(networks)
+        textdomain "network"
+
+        @networks = networks
+      end
+
+      def title
+        _("Wireless Available Networks")
+      end
+
+      def contents
+        networks_table
+      end
+
+    protected
+
+      def min_width
+        60
+      end
+
+    private
+
+      def networks_table
+        @table ||= Y2Network::Widgets::WirelessNetworks.new(@networks)
+      end
+    end
+  end
+end
