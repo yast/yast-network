6e62ba770163bc1c0f37d0dccf17f77431f80649
Imobach González Sosa <igonzalezsosa@suse.com>
2021-04-26 09:55:59 +0100

    Update from code review

--- src/lib/y2network/wicked/config_reader.rb
+++ src/lib/y2network/wicked/config_reader.rb
@@ -51,9 +51,10 @@ module Y2Network
         Yast::Host.Read
 
         initial_config = Config.new(source: :wicked)
+        issues_list = Y2Issues::List.new
 
         network_config = SECTIONS.reduce(initial_config) do |current_config, section|
-          send("read_#{section}", current_config)
+          send("read_#{section}", current_config, issues_list)
         end
 
         log.info "Sysconfig reader result: #{network_config.inspect}"
@@ -65,8 +66,9 @@ module Y2Network
       # Reads the network interfaces
       #
       # @param config [Y2Network::Config] Initial configuration object
+      # @param _issues_list [Y2Issues::List] Issues list
       # @return [Y2Network::Config] A new configuration object including the interfaces
-      def read_interfaces(config)
+      def read_interfaces(config, _issues_list)
         config.update(
           interfaces:   interfaces_reader.interfaces,
           s390_devices: interfaces_reader.s390_devices
@@ -76,8 +78,10 @@ module Y2Network
       # Reads the connections
       #
       # @param config [Y2Network::Config] Initial configuration object
+      # @param issues_list [Y2Issues::List] Issues list
       # @return [Y2Network::Config] A new configuration object including the connections
-      def read_connections(config)
+      def read_connections(config, issues_list)
+        connection_configs_reader = ConnectionConfigsReader.new(issues_list)
         connections = connection_configs_reader.connections(config.interfaces)
         missing_interfaces = find_missing_interfaces(
           connections, config.interfaces
@@ -91,16 +95,18 @@ module Y2Network
       # Reads the drivers
       #
       # @param config [Y2Network::Config] Initial configuration object
+      # @param _issues_list [Y2Issues::List] Issues list
       # @return [Y2Network::Config] A new configuration object including the connections
-      def read_drivers(config)
+      def read_drivers(config, _issues_list)
         config.update(drivers: interfaces_reader.drivers)
       end
 
       # Reads the routing information
       #
       # @param config [Y2Network::Config] Initial configuration object
+      # @param _issues_list [Y2Issues::List] Issues list
       # @return [Y2Network::Config] A new configuration object including the routing information
-      def read_routing(config)
+      def read_routing(config, _issues_list)
         routing_tables = find_routing_tables(config.interfaces)
         routing = Routing.new(
           tables:       routing_tables,
@@ -113,16 +119,18 @@ module Y2Network
       # Reads the DNS information
       #
       # @param config [Y2Network::Config] Initial configuration object
+      # @param _issues_list [Y2Issues::List] Issues list
       # @return [Y2Network::Config] A new configuration object including the DNS configuration
-      def read_dns(config)
+      def read_dns(config, _issues_list)
         config.update(dns: Y2Network::Wicked::DNSReader.new.config)
       end
 
       # Returns the Hostname configuration
       #
       # @param config [Y2Network::Config] Initial configuration object
+      # @param _issues_list [Y2Issues::List] Issues list
       # @return [Y2Network::Config] A new configuration object including the hostname information
-      def read_hostname(config)
+      def read_hostname(config, _issues_list)
         config.update(hostname: Y2Network::Wicked::HostnameReader.new.config)
       end
 
@@ -133,13 +141,6 @@ module Y2Network
       # @return [InterfacesReader] Interfaces reader
       def interfaces_reader
         @interfaces_reader ||= Y2Network::Wicked::InterfacesReader.new
-      end
-
-      # @return [Y2Network::ConnectionConfigsCollection] Connection configurations collection
-      def connection_configs_reader
-        @connection_configs_reader ||= Y2Network::Wicked::ConnectionConfigsReader.new(
-          issues_list
-        )
       end
 
       # Adds missing interfaces from connections
@@ -218,10 +219,6 @@ module Y2Network
         @sysctl_config_file = CFA::SysctlConfig.new
         @sysctl_config_file.load
         @sysctl_config_file
-      end
-
-      def issues_list
-        @issues_list ||= Y2Issues::List.new
       end
     end
   end
