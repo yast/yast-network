1d011b523ab7225f3c4999f6999e6c8cfa149aba
Martin Vidner <mvidner@suse.cz>
2022-06-07 15:37:07 +0200

    Rubify YCP zombies.

--- src/include/network/services/dns.rb
+++ src/include/network/services/dns.rb
@@ -498,18 +498,13 @@ module Yast
       sl = NonEmpty(Builtins.splitstring(value, " ,\n\t"))
       error = ""
 
-      Builtins.foreach(sl) do |s|
-        if !Hostname.CheckDomain(s)
-          # Popup::Error text
-          error = Ops.add(
-            Ops.add(
-              Builtins.sformat(_("The search domain '%1' is invalid."), s),
-              "\n"
-            ),
-            Hostname.ValidDomain
-          )
-          break
-        end
+      sl.each do |s|
+        next if Hostname.CheckDomain(s)
+
+        # TRANSLATORS: Popup::Error text
+        error = Builtins.sformat(_("The search domain '%1' is invalid."), s) +
+          "\n" + Hostname.ValidDomain
+        break
       end
 
       if error != ""
