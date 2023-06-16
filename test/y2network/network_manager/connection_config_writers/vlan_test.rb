67c64ba7f4ed76158e1c91b026a7a05a6448da3e
Michal Filka <mfilka@suse.cz>
2023-06-16 09:50:59 +0200

    Updated testsuite

---
+++ test/y2network/network_manager/connection_config_writers/vlan_test.rb
@@ -0,0 +1,45 @@
+# Copyright (c) [2021] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../../test_helper"
+require "y2network/network_manager/connection_config_writers/vlan"
+require "cfa/nm_connection"
+require "y2network/boot_protocol"
+require "y2network/startmode"
+require "y2network/connection_config/vlan"
+
+describe Y2Network::NetworkManager::ConnectionConfigWriters::Vlan do
+  subject(:handler) { described_class.new(file) }
+  let(:file) { CFA::NmConnection.new("bond0.nm_connection") }
+
+  let(:conn) do
+    Y2Network::ConnectionConfig::Vlan.new.tap do |c|
+      c.interface = "eth0.1006"
+      c.vlan_id = 1006
+    end
+  end
+
+  describe "#write" do
+    it "sets device and IP relevant attributes" do
+      handler.write(conn)
+      expect(file.vlan["type"]).to eql("vlan")
+      expect(file.vlan["id"]).to eql("1006")
+    end
+  end
+end
